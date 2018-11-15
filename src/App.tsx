5ad862e34c9321f4b2dfca3fc9778cbcd18eb3d6
Yogesh Gaonkar <yogesh.gaonkar@corp.intelliswift.com>
2018-11-15 11:52:14 +0530

    updated lib version

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,14 @@ import { FlexButton } from 'flexible-ui-comps'
 import * as React from 'react';
 import './App.css';
 
-
-import logo from './logo.svg';
-
-
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Intelliswift Study Portal</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> <FlexButton /> and save to reload.
-        </p>
+        <FlexButton />
       </div>
     );
   }
